b83b2436e9d53366d9b436e191a2013941884056
NoamAbutbul <135932038+NoamAbutbul@users.noreply.github.com>
2024-12-26 02:53:36 +0200

    clean code

--- src/main/java/com/noama/GenreIdentificationServer/Application.java
+++ src/main/java/com/noama/GenreIdentificationServer/Application.java
@@ -1,57 +1,11 @@
 package com.noama.GenreIdentificationServer;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
-public class Application
-{    
-    public static void main(String[] args) throws Exception
-    {
+public class Application {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args); // הרצת השרת
-        
-//         Path DATA_PATH = Paths.get("src/main/java/com/noama/GenreIdentificationServer/Data/");
-//         File i = DATA_PATH.resolve("testingset.csv").toFile();
-//         System.out.println("================================================================= Canread? " + i.canRead());
-        
-        
-//        MusicGenreClassifier MGC = new MusicGenreClassifier();
-//        MGC.loadModel();
-//        
-//        MGC.loadData();
-        
-        
-
-        
-
-
-
-//        MGC.loadData();
-//        MGC.createNetwork();
-//        MGC.loadModel();
-//        System.out.println("Prediction: " + MGC.predictGenre());
-
-        
-
-        
-        //CSVManage.normalizeData();
-        
-//        MusicGenreClassifier MGC = new MusicGenreClassifier();
-
-//         MusicGenreClassifier MGC = new MusicGenreClassifier();
-//         
-//         InputStream inputStream = new FileInputStream("/Users/noamabutbul/Downloads/videoplayback.wav");
-//         MGC.predictGenre(inputStream);
-
-
-
-
-
     }
-
 }
-
- 
